c7ec2f18f2b900855ab0cc2cccaa9f32d15f409a
ming.ma <ming.ma@upai.com>
2014-05-02 00:08:56 +0800

    ImageUtils 新增 decodePNGStreamAsRawImage 方法，基于 pngj 解码png 图片，目前只处理 PNG RGB8 & PNG RGBA8 类型的PNG图片

--- src/main/java/org/jocean/image/ImageUtils.java
+++ src/main/java/org/jocean/image/ImageUtils.java
@@ -5,10 +5,17 @@ package org.jocean.image;
 
 import java.io.InputStream;
 
+import org.jocean.idiom.block.BlockUtils;
+import org.jocean.idiom.block.IntsBlob;
+import org.jocean.idiom.block.WriteableInts;
 import org.jocean.idiom.pool.BytesPool;
 import org.jocean.idiom.pool.IntsPool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import ar.com.hjg.pngj.IImageLine;
+import ar.com.hjg.pngj.ImageLineInt;
+import ar.com.hjg.pngj.PngReader;
 
 import com.alibaba.simpleimage.codec.jpeg.JPEGDecoder;
 import com.alibaba.simpleimage.io.ImageBitsInputStream;
@@ -33,9 +40,39 @@ public class ImageUtils {
         final IntsPool intsPool;
     }
     
-    public static RawImage decodeStreamAsRawImage(final Context ctx, final InputStream is) 
+    public static RawImage decodeJPEGStreamAsRawImage(final Context ctx, final InputStream is) 
             throws Exception {
         final JPEGDecoder decoder = new JPEGDecoder(ctx.bytesPool, new ImageBitsInputStream(is));
         return decoder.decode(ctx.intsPool);
+    }
+    
+    public static RawImage decodePNGStreamAsRawImage(final Context ctx, final InputStream is) 
+            throws Exception {
+        final PngReader pngr = new PngReader(is);
+        final int channels = pngr.imgInfo.channels;
+        if (channels < 3 || pngr.imgInfo.bitDepth != 8) {
+            LOG.warn("This method is for RGB8/RGBA8 images");
+            return null;
+        }
+        final WriteableInts output = BlockUtils.createWriteableInts(ctx.intsPool);
+        
+        while(pngr.hasMoreRows()) {
+            final IImageLine l1 = pngr.readRow();
+            final int[] scanline = ((ImageLineInt) l1).getScanline(); // to save typing
+            for (int j = 0; j < pngr.imgInfo.cols; j++) {
+                final int R = scanline[j * channels] & 0xff;
+                final int G = scanline[j * channels+1] & 0xff;
+                final int B = scanline[j * channels+2] & 0xff;
+                output.write( 0xff000000 | (R << 16) | (G << 8) | B );
+            }
+        }
+        final IntsBlob ints = output.drainToIntsBlob();
+        try {
+            return new RawImage(pngr.imgInfo.cols, pngr.imgInfo.rows, ints);
+        }
+        finally {
+            ints.release();
+            pngr.close();
+        }
     }
 }
